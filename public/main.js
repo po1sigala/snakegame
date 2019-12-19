a3f75d9302aa9542b53028f224fbbc591ade9ad0
Your Name <alexsigala87@gmail.com>
2019-12-18 17:21:22 -0800

    mapped out how the js logic will go

--- public/main.js
+++ public/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+    let score;
+    let time;
+
     console.log("logic loaded");
+
+    // functions
+
+    //create function which determines direction to head next based on user input
+    //creat function which turns the next box black
+    //create fucntion which checks to see if fucntion is already black aka biting itself
+    //create timer to see how long you last
+    //create timed function which calls for a dot to appear
+    //create function which displays a dot to be eaten
+    //create function which increases score when dot is eaten
 });
